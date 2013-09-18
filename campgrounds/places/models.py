ece2db508b7cbdce21c789c7654d44d4171f5c8f
fatiherikli <fatih@hipo.biz>
2013-09-19 02:13:44 +0300

    Initial commit

---
+++ campgrounds/places/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import models
+from django.utils.encoding import smart_unicode
+
+
+class Campground(models.Model):
+    """Holds camp areas"""
+    title = models.CharField(max_length=255)
+    city = models.CharField()
+    location = models.TextField()
+    address = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __unicode__(self):
+        return smart_unicode(self.title)
+
+
+class Rating(models.Model):
+    """Holds the ratings of a campground"""
+    campground = models.ForeignKey(Campground)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    vote = models.IntegerField()
+    comment = models.TextField()
+
+    def __unicode__(self):
+        return smart_unicode(self.comment)
+
+
+class Photo(models.Model):
+    """Holds photos of a campground"""
+    title = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __unicode__(self):
+        return smart_unicode(self.title)
---
+++ campgrounds/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(
+        template_name="index.html"), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
