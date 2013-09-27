a3236667c4912c4fc337d765a554ee52241e9772
fatiherikli <fatih@hipo.biz>
2013-09-27 04:26:24 +0300

    Added notification model

--- campgrounds/profiles/models.py
+++ campgrounds/profiles/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.encoding import smart_unicode
 
 
 class User(AbstractUser):
@@ -11,3 +14,20 @@ class User(AbstractUser):
             'hash': hashlib.md5(self.email.lower()).hexdigest(),
             'parameters': urllib.urlencode({'s':str(size)})
         }
+
+
+NOTIFICATION_TYPES = (
+    (1, 'Approved your campground.'),
+)
+
+class Notification(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  related_name="notifications")
+    notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
+    is_seen = models.BooleanField(default=False)
+    object_id = models.IntegerField()
+
+    def __unicode(self):
+        return smart_unicode("%s -> %s" % (self.get_notification_type_display(),
+                                           self.recipient))
