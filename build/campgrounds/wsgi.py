1f79193af3941511fb352f27d6d677333132178e
Yigit Guneli <pkd@hipo.biz>
2013-09-30 15:20:15 +0300

    setting path on wsgi.py

--- build/campgrounds/wsgi.py
+++ build/campgrounds/wsgi.py
@@ -15,6 +15,10 @@ framework.
 """
 import os
 
+site.addsitedir(os.path.join(os.path.realpath(os.path.dirname(__file__)), "../../../../lib/python2.7/site-packages"))
+sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), "../"))
+
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
